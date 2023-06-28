7bcf8c250b8a3405a660040661039000c5a90e5c
junjunghasudungan <junjunghasudungan43@gmail.com>
2023-06-28 09:20:24 +0700

    persiapan pembuatan create diagnosa front-end

--- app/Http/Livewire/Diagnosa.php
+++ app/Http/Livewire/Diagnosa.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Gejala;
 use Livewire\Component;
 
 class Diagnosa extends Component
 {
+    public  $create_modal = false,
+            $gejala,
+            $user_id;
+
+    public $all_gejala = [];
+
+    public function mount()
+    {
+        $this->all_gejala = Gejala::all();
+    }
     public function render()
     {
+        // dd($this->all_gejala);
+
         return view('livewire.diagnosa');
+    }
+
+    public function openCreateModal()
+    {
+        $this->create_modal = true;
+    }
+
+    public function createDiagnosa()
+    {
+        $this->openCreateModal();
+
+    }
+
+    public function storeDiagnosa()
+    {
+        $this->openCreateModal();
+    }
+
+    public function closeCreateModal()
+    {
+        $this->create_modal = false;
     }
 }
